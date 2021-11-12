478317aa193c1860ca64fa746b31f98431765077
Patrik Nemeth <patriknemethpn@gmail.com>
2021-11-12 14:08:54 +0100

    Apply assignment updates and bug fixes
    The original assignment had some bugs in the testing scripts, CMake and errors in the assignment text itself.

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -12,7 +12,7 @@ echo "Reference vs line"
 python3 ${SCRIPT_ROOT_PATH}/compare.py cmp_ref.npz cmp_line.npz  || VALID=0
 
 echo "Reference vs batch"
-python3 ${SCRIPT_ROOT_PATH}/compare.py cmp_ref.npz cmp_line.npz || VALID=0
+python3 ${SCRIPT_ROOT_PATH}/compare.py cmp_ref.npz cmp_batch.npz || VALID=0
 
 
 echo "Batch vs line"
